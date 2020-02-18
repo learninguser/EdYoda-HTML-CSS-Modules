461aeba8cca41c615d37d76baa64c273a8676409
Pavan <akulapavankumar@outlook.com>
2020-02-18 21:34:57 +0100

    Added sessions 5-7

--- Python-Django/Python/task-1.py
+++ Python-Django/Python/task-1.py
@@ -1,15 +1,25 @@
-a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279]
-b = [832, 570, 148, 998, 533, 561, 455, 147, 894, 279]
+# a = [832, 998, 148, 570, 533, 561, 894, 147, 455, 279]
+# b = [832, 570, 148, 998, 533, 561, 455, 147, 894, 279]
 
-count = 0
-temp = []
+# count = 0
+# temp = []
 
-for idx, val in enumerate(a):
-    if val == b[idx]:
-        continue
-    else:
-        tInd = idx
-        temp.append([tInd, val, b[tInd]])
-        count = count + 1
-        if count == 3:
-            break
+# for idx, val in enumerate(a):
+#     if val == b[idx]:
+#         continue
+#     else:
+#         tInd = idx
+#         temp.append([tInd, val, b[tInd]])
+#         count = count + 1
+#         if count == 3:
+#             break
+l1 = [[1,2,3],[4,5,6],[7,8,9]]
+l2 = [[100,200,300],[400,500,600],[700,800,900]]
+l4 = []
+
+for idx in range(len(l1)):
+    l3 = []
+    for idy in range(len(l1[0])):
+        l3.append(l2[idx][idy] + l1[idx][idy])
+    l4.append(l3)
+print(l4)
