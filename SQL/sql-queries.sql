50755387297fd46e977b3f96f74d4ee9b3401136
Pavan Kumar Akula <akulapavankumar@outlook.com>
2020-05-27 13:27:22 +0200

    added django files

--- SQL/sql-queries.sql
+++ SQL/sql-queries.sql
@@ -1,3 +1,15 @@
-create table author(id int, first_name varchar(100), last_name varchar(100), email varchar(100), followers int, profile_pic varchar(250)); 
+-- SELECT DISTINCT (e.first_name), e.last_name, t.title
+-- FROM employees AS e 
+-- INNER JOIN titles AS t 
+-- ON e.emp_no = t.emp_no
+-- INNER JOIN dept_emp AS d_emp
+-- ON e.emp_no = d_emp.emp_no
+-- INNER JOIN departments AS d
+-- ON d.dept_no = d_emp.dept_no
+-- LIMIT 10;
 
-create table category(id int, name varchar(100), description varchar(500), followers int);
+SELECT DISTINCT first_name, last_name, titles.title 
+FROM employees
+INNER JOIN titles
+ON employees.emp_no = titles.emp_no
+LIMIT 10;
