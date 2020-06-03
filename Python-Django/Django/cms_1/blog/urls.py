beeaf667b8c7e77b09c7b85c851971095ea22d79
Pavan Kumar Akula <pavan.akula@ad.fhr.fraunhofer.de>
2020-06-03 07:05:31 +0200

    Added templates and redirection

--- Python-Django/Django/cms_1/blog/urls.py
+++ Python-Django/Django/cms_1/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from blog import views
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
+    path('blog/<int:id>',views.post_details),
 ]
--- Python-Django/Django/cms_1/blog/views.py
+++ Python-Django/Django/cms_1/blog/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.filter(status='P')
+    return render(request, 'blog/index.html',context={'posts':posts})
+
+# Create your views here.
+def post_details(request,id):
+    try:
+        posts = Post.objects.filter(id=id)
+        return render(request, 'blog/test.html',context={'posts':posts})
+    except:
+        return HttpResponse("The page you're looking doesn't exsist")
